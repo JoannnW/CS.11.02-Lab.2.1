62b81f617a927577607b03c98c86d20002815f16
joanwongclgmail.com <joanwongcl@gmail.com>
2023-09-21 17:06:06 +0800

    It works now

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,61 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int result = add(3,8);
+        System.out.println(result);
+
+        int result2 = add(3,8,4,9);
+        System.out.println(result2);
+
+        String result3 = morningGreeting("Toby Fox");
+        System.out.println(result3);
+
+        String result4 = afternoonGreeting("Max Miller");
+        System.out.println(result4);
+
+        String result5 = triple("oohbaby");
+        System.out.println(result5);
+
+        double result6 = half(17);
+        System.out.println(result6);
+
+        int result7 = roundPositiveValueToNearestInteger(8.5);
+        System.out.println(result7);
+
+        int result8 = roundNegativeValueToNearestInteger(-8.5);
+        System.out.println(result8);
     }
 
     // 1. add
-    public static int add(int a,int b){
-        int result = add(3,8);
+    public static int add(int a,int b) {
         return a+b;
     }
-
     // 2. add
-    public static int add(int a,int b,int c,int d){
-        int result = add(5,7,6,6);
-        return a+b+c+d;
+    public static int add(int a,int b,int c,int d) {
+        return add(add(a,b),add(c,d));
     }
-
     // 3. morningGreeting
-    public static String morningGreeting(String name){
-        return "早上好， "+morningGreeting("Toby Fox")+"!";
+    public static String morningGreeting(String mornining) {
+        return "早上好, "+mornining+"!";
     }
-
     // 4. afternoonGreeting
-    public static String afternoonGreeting(String name){
-        return "下午好 " + afternoonGreeting("Mac Miller") + "!";
-
-        // 5. triple
-        public static String triple(String name){
-            return triple+triple+triple;
-        }
-        // 6. half
-        public static double half(int n){
-            return n/2.0;
-        }
-        // 7. roundPositiveValueToNearestInteger
-        public static int roundNegativeValueToNearestInteger(double pValue){
-            return (int) (pValue + 0.5);
-        }
-        // 8. roundNegativeValueToNearestInteger
-        public static int roundNegativeValueToNearestInteger(double nValue){
-            return (int) (nValue - 0.5);
-        }
+    public static String afternoonGreeting(String afternoon) {
+        return "下午好, "+afternoon+"!";
     }
-
+    // 5. triple
+    public static String triple(String three) {
+        return three+three+three;
+    }
+    // 6. half
+    public static double half(int a) {
+        return a/2.0;
+    }
+    // 7. roundPositiveValueToNearestInteger
+    public static int roundPositiveValueToNearestInteger(double a) {
+        return (int) Math.round(a);
+    }
+    // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger(double b) {
+        return (int) Math.round(b);
+    }
 }
